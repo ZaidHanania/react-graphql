35a6e50a53e051a38cbe40950bc4bc5e7a90097c
Zaid Hanania <zaid.hanania@gmail.com>
2018-10-24 14:42:34 -0400

    feat: added prisma and yoga flow

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: '.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, info => {
+    console.log(`Server is now running on port ${info.port}`)
+})
---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,15 @@
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: check if authenticated
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args,
+            }
+        }, info)
+
+        return item
+    }
+};
+
+module.exports = Mutations;
